3b5d1fda58b9b3cef96d81d3d73d58369edef6e2
khalid-kamal07 <khalidbruda07@gmail.com>
2025-08-24 10:05:27 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+
+//Chapter No-1
+
+// alert("Welcome to our website!");
+
+
+// alert("Error! Please enter a valid password");
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+
+// alert("Message 1");
+// alert("Message 2");
+// alert("Message 3");
+
+// alert("Hello... I can run JS through my web browser's console");
+
+// alert("This is an alert in my project!");
+
+// <head>
+//     <script>alert("Script in head!");</script>
+// </head>
+
+
+// <body>
+//     <script>alert("Script before HTML content!");</script>
+//     <!-- Your HTML content -->
+// </body>
+
+// <body>
+//     <!-- Some HTML content -->
+//     <div>
+//         <script>alert("Script inside HTML content!");</script>
+//     </div>
+//     <!-- More HTML content -->
+// </body>
+
+
+// <body>
+//     <!-- All your HTML content -->
+//     <script>alert("Script after HTML content!");</script>
+// </body>
+
+
+
+//Chapter No-2
+
+
+// 1. Declare a variable called username
+var username;
+
+// 2. Declare a variable called myName & assign it your full name
+var myName = "John Doe";
+
+// 3. Write script to:
+// a) Declare a JS variable titled message
+var message;
+
+// b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in alert box
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show in alert boxes
+var studentName = "John Doe";
+var studentAge = "15 years old";
+var course = "Certified Mobile Application Development";
+
+alert(studentName);
+alert(studentAge);
+alert(course);
+
+
+
+
+// Question 8: Write a script to display this in browser through JS
+
+```html
+<script>
+  document.write("Yah! I can write HTML content through JavaScript");
+</script>
+```
+
+
+// Question 9: Store following string in a variable and show in alert and browser through JS
+
+```html
+<script>
+  var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+  alert(message);
+  document.write(message);
+</script>
+
+
